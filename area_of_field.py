c91899bf87d5dcbe233c6bf72c03bf7a0857cabf
Geethika Lakshmi <59430375+Geethika13@users.noreply.github.com>
2020-05-03 13:23:33 +0530

    Update area_of_field.py
    The task is to find the area of the field where the user inputs the dimensions of the field and the result is displayed in acres with two decimal places.

--- area_of_field.py
+++ area_of_field.py
@@ -5,10 +5,12 @@ Created on Wed Apr 29 19:58:57 2020
 
 @author: geethikalakshmi
 """
-
+#input the dimensions of the room
 length=float(input('enter the length of the field'))
 width=float(input('enter the width of the field'))
+#use the formula of area
 area=length*width
+#convert aquare foot values into acres
 sqrt_per_acre=43560
 acre=area/sqrt_per_acre
 print('area of field is:',acre,'acres')
