e33f9c531dbd6ed39f6c8dcc26aa7c43a6a18d52
Geethika Lakshmi <59430375+Geethika13@users.noreply.github.com>
2020-05-03 13:20:59 +0530

    Update area_of_a_room.py
    The task is to find the area of the room where user inputs the dimensions of the room

--- area_of_a_room.py
+++ area_of_a_room.py
@@ -8,6 +8,7 @@ Created on Wed Apr 29 19:39:48 2020
 #to find tbe area of the room 
 length=float(input('enter the length of the room:'))
 breadth=float(input('enter the breadth of the room:'))
+#use the formula of area
 area=length*breadth
-#read the area
+#display the result
 print('area of the room is:',area,'sqaure feet')
