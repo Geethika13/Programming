ad8577b3745720f622a99d61a5b92295ad41c869
Geethika Lakshmi <59430375+Geethika13@users.noreply.github.com>
2020-05-03 13:16:33 +0530

    Update bottle_depo.py
    The task is to refund the amount to the people who has the bottle containers with the capacity of 1litre and more than 1 litre to return for the recycling.The amount refunded for the 1 litre and more than a litre is $0.10 and $0.25 respectively and displaying the refunded amount with two decimal places.

--- bottle_depo.py
+++ bottle_depo.py
@@ -5,10 +5,12 @@ Created on Wed Apr 29 20:08:27 2020
 
 @author: geethikalakshmi
 """
+#defining the refund values per container
 less_depo=0.10
 more_depo=0.25
-
+#input the values by the user 
 less=float(input('how many 1 litre containers do you have?'))
 more=float(input('how many more than 1 ltre containers do you have?'))
+#displaying total refund value with two decimal places
 refund=less*less_depo+more*more_depo
 print('total refund is $.2f.'% refund)
