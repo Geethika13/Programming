136335a332586f05088c7d24a0dd55175eaeef2d
Geethika Lakshmi <59430375+Geethika13@users.noreply.github.com>
2020-05-03 13:33:27 +0530

    Update tax_and_tip.py
    The task is to print the tax and tip amount added to the total amount of the meal purchased by an individual.I shall print the value with two decimal places.

--- tax_and_tip.py
+++ tax_and_tip.py
@@ -5,13 +5,15 @@ Created on Fri May  1 20:35:15 2020
 
 @author: geethikalakshmi
 """
-
+#dfining the values of taxes and tips 
 tax_rate=0.18
 tip_rate=0.10
-
+#input the values
 cost=float(input('enter the cost of the meal:'))
+#calculating the tax,tip and cost
 tax=cost*tax_rate
 tip=cost*tip_rate
 total=cost+tax+tip
+#displaying the result
 print('the tax is %.2f and the tip is %.2f making the total %.2f:'%\
       (cost,tax,tip,total))
